1a9f07deb00dcf3b56da81e6aa232c59addbabe0
IanCastlee <castillo321ian@gmail.com>
2025-04-03 15:21:37 +0800

    ok na to

--- api/connect.js
+++ api/connect.js
@@ -1,27 +1,10 @@
-import mysql from "mysql";
-
-export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "faithbook",
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection failed: ", err);
-  } else {
-    console.log("Connected to MySQL database");
-  }
-});
-
 // import mysql from "mysql";
 
 // export const db = mysql.createConnection({
-//   host: "b6nmsad8p5ce813ahsgg-mysql.services.clever-cloud.com",
-//   user: "ugncgv8zjmup0doe",
-//   password: "FIPjWtRp6aPw03PO8ZQ6",
-//   database: "b6nmsad8p5ce813ahsgg",
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "faithbook",
 // });
 
 // db.connect((err) => {
@@ -31,3 +14,20 @@ db.connect((err) => {
 //     console.log("Connected to MySQL database");
 //   }
 // });
+
+import mysql from "mysql";
+
+export const db = mysql.createConnection({
+  host: "b6nmsad8p5ce813ahsgg-mysql.services.clever-cloud.com",
+  user: "ugncgv8zjmup0doe",
+  password: "FIPjWtRp6aPw03PO8ZQ6",
+  database: "b6nmsad8p5ce813ahsgg",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed: ", err);
+  } else {
+    console.log("Connected to MySQL database");
+  }
+});
